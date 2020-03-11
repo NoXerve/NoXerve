168a9caff006503009284d68efd1609ef356ce0a
yves <magneticchen@gmail.com>
2020-03-11 22:28:15 +0800

    rearranged dir structures.

---
+++ src/noxerve_agent/nodejs/node/sercured_node.js
@@ -0,0 +1,12 @@
+/**
+ * @file NoXerveAgent secured node file. [secured_node.js]
+ * @author NOOXY <thenooxy@gmail.com>
+ * @author noowyee <magneticchen@gmail.com>
+ * @copyright 2019-2020 NOOXY. All Rights Reserved.
+ */
+
+ 'use strict';
+
+ /**
+  * @module SecuredNode
+  */
