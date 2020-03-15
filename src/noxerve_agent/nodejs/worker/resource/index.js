cf24ef98460d32341d03f02be73e4f2e1a34c791
yves <magneticchen@gmail.com>
2020-03-16 01:33:44 +0800

    more update on resource module.

--- src/noxerve_agent/nodejs/worker/resource/index.js
+++ src/noxerve_agent/nodejs/worker/resource/index.js
@@ -93,49 +93,99 @@ Resource.prototype.isClaimed = function() {
 // Each example is a different implementation.
 
 // Example 1 "executeConcurrentTask"
-// task_name: read
-// task_parameter: {path: "reversi.game_record", data: 1231231}
+// resource_function_name: read
+// resource_function_parameter: {path: "reversi.game_record", data: 1231231}
 // scope_name: user.tom
-// scope_interation: 223
+// scope_interation_number: 223
 
 // Example 2 "executeConcurrentTask"
 // (this one is not a good implementation since too much scope
 // name , and the hierarchy dependencies of file system may cause some troubles.)
-// task_name: write
-// task_parameter: {file_name: "record_1.txt", data: 1334234}
+// resource_function_name: write
+// resource_function_parameter: {file_name: "record_1.txt", data: 1334234}
 // scope_name: user.tom.game_data
-// scope_interation: 321
+// scope_interation_number: 321
 
 // Example 3 "executeTask"
 // ("executeTask" not gonna emit writeTaskParameterRecord)
-// task_name: broadcast_message
-// task_parameter: "message 1"
+// resource_function_name: broadcast_message
+// resource_function_parameter: "message 1"
 // scope_name: channel_1
-// scope_interation: null
+// scope_interation_number: null
+
+// // [Flag] Unfinished annotation.
+// Resource.prototype.createScope = function() {
+//   this._event_listeners['']();
+// }
+//
+// // [Flag] Unfinished annotation.
+// Resource.prototype.destroyScope = function() {
+//   this._event_listeners['']();
+// }
 
 // [Flag] Unfinished annotation.
-Resource.prototype.createScope = function() {
-  this._event_listeners['']();
+Resource.prototype.resumeMe = function() {
+
 }
 
 // [Flag] Unfinished annotation.
-Resource.prototype.destroyScope = function() {
-  this._event_listeners['']();
+Resource.prototype.suspendMe = function() {
+
 }
 
 // [Flag] Unfinished annotation.
-Resource.prototype.writeConcurrentTaskParameterRecord = function(task_name, task_parameter, scope_name, scope_interation) {
-  this._event_listeners['']();
+Resource.prototype.recoverFromRemoteSnapshot = function(callback) {
+  callback();
 }
 
 // [Flag] Unfinished annotation.
-Resource.prototype.readConcurrentTaskParameterRecord = function(task_name, task_parameter, scope_name, scope_interation) {
-  this._event_listeners['']();
+Resource.prototype.handleRecoveryFromSnapshot = function(handler) {
+
 }
 
 // [Flag] Unfinished annotation.
-Resource.prototype.deleteConcurrentTaskParameterRecord = function(task_name, task_parameter, scope_name, scope_interation) {
-  this._event_listeners['']();
+Resource.prototype.handleScopeInformationWrite = function(handler) {
+  handler(scope_name, scope_interation_number, callback);
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleScopeInformationRead = function(handler) {
+  handler(scope_name, callback);
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleScopeInformationDelete = function(handler) {
+  handler(scope_name, callback);
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleConcurrentFunctionRecordWrite = function(handler) {
+
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleConcurrentFunctionRecordRead = function(handler) {
+
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleConcurrentFunctionRecordDelete = function(handler) {
+
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleConcurrentYieldingRecordWrite = function(handler) {
+
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleConcurrentYieldingRecordRead = function(handler) {
+
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleConcurrentYieldingRecordDelete = function(handler) {
+
 }
 
 // handleConcurrentTask('write', (yielding_handler_parameter, handle_yielding, start_yielding)=> {
@@ -146,14 +196,25 @@ Resource.prototype.deleteConcurrentTaskParameterRecord = function(task_name, tas
 //  });
 // });
 // [Flag] Unfinished annotation.
-Resource.prototype.handleConcurrentTask = function(task_name, task_handler) {
-  task_handler(task_parameter, scope_name, finish, yield_data, is_master);
+Resource.prototype.define = function(resource_function_name, resource_function) {
+  resource_function(resource_function_parameter, scope_name, finish, yield_data);
   result_callback();
 }
 
 // [Flag] Unfinished annotation.
-Resource.prototype.handleTask = function(task_name, task_handler) {
-  task_handler(task_parameter, scope_name, finish, yield_data);
+Resource.prototype.defineConcurrently = function(resource_function_name, resource_function) {
+  resource_function(resource_function_parameter, scope_name, finish, yield_data, is_master);
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleYielding = function(field_name, resource_function) {
+  resource_function(resource_function_parameter, scope_name, finish, yield_data);
+  result_callback();
+}
+
+// [Flag] Unfinished annotation.
+Resource.prototype.handleYieldingConcurrently = function(field_name, resource_function) {
+  resource_function(resource_function_parameter, scope_name, finish, yield_data, is_master);
 }
 
 // ***** API for claimed workers end *****
@@ -168,14 +229,24 @@ Resource.prototype.handleTask = function(task_name, task_handler) {
 // -> emit to resource worker peers.(include resource worker whatsoever itself)
 // -> resource worker peers proceed.(include resource worker whatsoever itself)
 // [Flag] Unfinished annotation.
-Resource.prototype.executeConcurrentTask = function(task_name, task_parameter, scope_name, callback) {
+Resource.prototype.call = function(resource_function_name, resource_function_argument, scope_name, resource_function_callback) {
   this._event_listeners['']();
 }
 
 // [Flag] Unfinished annotation. Non-blocking task.
-Resource.prototype.executeTask = function(task_name, task_parameter, scope_name, callback) {
+Resource.prototype.callConcurrently = function(resource_function_name, resource_function_argument, scope_name, resource_function_callback) {
   this._event_listeners['']();
 }
+
+Resource.prototype.startYielding = function(field_name, yielding_start_argument, scope_name, yielding_start_callback) {
+  this._event_listeners['']();
+}
+
+// [Flag] Unfinished annotation. Non-blocking task.
+Resource.prototype.startYieldingConcurrently = function(field_name, yielding_start_argument, scope_name, yielding_start_callback) {
+  this._event_listeners['']();
+}
+
 
 /**
  * @callback module:Resource~callback_of_on
