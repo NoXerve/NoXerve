45f8142e09f9cdab2aa633d3b9b6e42b3f63e360
C.W. Chien <dppss92132@gmail.com>
2020-10-03 14:58:31 +0800

    make ACORN safer

--- src/noxerve_agent/nodejs/protocol/protocols/worker/ACORN.js
+++ src/noxerve_agent/nodejs/protocol/protocols/worker/ACORN.js
@@ -1,7 +1,6 @@
 /**
- * @file The k-th orderAdditive Congruential Random Number (ACORN) generator. More details at http://acorn.wikramaratna.org/concept.html
+ * @file The k-th order Additive Congruential Random Number (ACORN) generator. More details at http://acorn.wikramaratna.org/concept.html
  * @author nooxy <thenooxy@gmail.com>
- * @author noowyee <magneticchen@gmail.com>
  * @author idoleat <dppss92132@gmail.com>
  * @copyright 2019-2020 nooxy. All Rights Reserved.
  */
@@ -16,15 +15,35 @@ function ACORN() {
     this.N = 1;
     //this.M = Math.pow(2, 30*precision_multiplier);
     this.M = 1073741824*this.precision_multiplier;
+    this.seed = undefined;
+    this.auto_correct_seed = true;
+
+    // constructor
+    function ACORN(precision_multiplier, order, auto_correct_seed) {
+        this.precision_multiplier = precision_multiplier;
+        this.K = order;
+        this.auto_correct_seed = auto_correct_seed;
+    }
 }
 
-const IsInputValid = function(seed, length) {
-    if(Number.isNaN(seed) || seed%2 === 0 ) {
-        console.log('Invalid seed. Must be odd.');
+ACORN.prototype.IsInputValid = function(seed, length) {
+    if(Number.isNaN(seed)) {
+        console.log('Invalid seed. Must be a number.');
         return false;
+    }
+    if(seed%2 === 0 ) {
+        if(this.auto_correct_seed) this.seed -= 1;
+        else {
+            console.log('Invalid seed. Must be odd.');
+            return false;
+        }
     }
     if(length < 1){
         console.log('Invalid length. Must greater than 1.');
+        return false;
+    }
+    if(!Number.isInteger(length)){
+        console.log('Invalid length. Must be integer.');
         return false;
     }
 
@@ -32,23 +51,26 @@ const IsInputValid = function(seed, length) {
 }
 
 /**
- * Modify precision and order. Default ones are 1 and 10000.
- * @param  {[type]} precision_multiplier Multipler to multiply the M value, which is 2^30 by default.
- * @param  {[type]} order                Order.
+ * Modify precision, order and option to auto correct seed.
+ * @param  {integer} precision_multiplier Multipler to multiply the M value, which is 2^30 by default. Default value is 1.
+ * @param  {integer} order                Order. Default is 10000.
+ * @param  {boolean} auto_correct_seed    An option to auto correct even seed. Default is true.
  */
-ACORN.prototype.setProperty = function(precision_multiplier, order) {
+ACORN.prototype.setProperty = function(precision_multiplier, order, auto_correct_seed) {
     this.precision_multiplier = precision_multiplier;
     this.K = order;
+    this.auto_correct_seed = auto_correct_seed;
 };
 /**
  * Generate an array from seed with specified length filled with number in [0,1). To avoid small first element, recommand to use the seed greater than 10^5.
- * @param  {number} seed   seed
+ * @param  {number} seed    Seed. Becareful that using the seed greater than Number.MAX_SAFE_INT will make the seed become zero. BigInt not supported yet.
  * @param  {integer} length Length of the returned array
- * @return {Array}        Generated numbers.
+ * @return {Array}          Generated numbers.
  */
 ACORN.prototype.random = function(seed, length) {
-    if(!IsInputValid(seed,length)) return undefined;
     seed = seed % this.M;
+    this.seed = seed;
+    if(!this.IsInputValid(this.seed,length)) return undefined;
     this.N = length+1;
 
     let Y1 = [];
@@ -67,11 +89,13 @@ ACORN.prototype.random = function(seed, length) {
     }
 
     Y2.shift(); // remove the first element, which is seed, to fit the length.
-    //Y2.forEach( (element, index) => { this[index] = element/this.M } , Y2);
     for(let i=0; i<length; i++){
-        Y2[i] = Y2[i]/this;
+        Y2[i] = Y2[i]/this.M;
     }
     return Y2;
 }
+
+//let ac = new ACORN();
+//console.log(ac.random(1000000, 10));
 
 module.exports = ACORN
